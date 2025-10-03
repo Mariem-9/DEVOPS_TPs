59c525d4fddacb75aa770f58de97dd5d616200a4
rami25 <ramibenmad4@gmail.com>
2025-10-03 15:53:30 +0100

    first commit

--- src/application/test.java
+++ src/application/test.java
@@ -5,6 +5,9 @@ public class test {
 	{
 		//Print msg
 		System.out.println("Nice");
+		System.out.println("rami25 was here");
+		System.out.println("rami25 print rami25 was here");
+		System.out.println("rami25 print rami25 pint rami25 was here");
 	}
 
 }
