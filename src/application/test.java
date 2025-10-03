38716342f71ccb5dad76f4943a4c068a1afac448
rami25 <ramibenmad4@gmail.com>
2025-10-03 16:27:25 +0100

    second commit

--- src/application/test.java
+++ src/application/test.java
@@ -4,10 +4,7 @@ public class test {
 	public static void main (String[] args) 
 	{
 		//Print msg
-		System.out.println("Nice");
 		System.out.println("rami25 was here");
-		System.out.println("rami25 print rami25 was here");
-		System.out.println("rami25 print rami25 pint rami25 was here");
 	}
 
 }
