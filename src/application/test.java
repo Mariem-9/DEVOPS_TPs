433dd01610a39d108d88ebf43ade17fb97e052fc
amine-ENSI <mohamedamine.hattay@ensi-uma.tn>
2025-10-03 16:52:28 +0200

    dev commit

--- src/application/test.java
+++ src/application/test.java
@@ -4,7 +4,7 @@ public class test {
 	public static void main (String[] args) 
 	{
 		//Print msg
-		System.out.println("Nice");
+		System.out.println("CE git exemple a été fait avec collaboration");
 	}
 
 }
